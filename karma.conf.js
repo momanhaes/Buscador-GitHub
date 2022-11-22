5359b9e15cf0a123f9161a76b0c7828442f4385a
Matheus Manhães <momanhaes@gmail.com>
2022-11-22 13:02:01 -0300

    feat: renomeia repo-search para styleguide

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/repo-search'),
+      dir: require('path').join(__dirname, './coverage/styleguide'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,15 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'repo-search'`, () => {
+  it(`should have as title 'styleguide'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('repo-search');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('repo-search app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('styleguide app is running!');
   });
 });
