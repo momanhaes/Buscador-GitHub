9b4c06b324ef93dbb5ec2a28e125b4e853febb70
Matheus Manhães <momanhaes@gmail.com>
2022-11-28 12:03:43 -0300

    feat

--- src/app/pages/home/home.content.ts
+++ src/app/pages/home/home.content.ts
@@ -7,13 +7,13 @@ export interface IHome {
 }
 
 export const HOME: IHome[] = [
-  {
-    theme: 'outline',
-    label: 'Portfolio',
-    icon: 'fa fa-user-circle',
-    route: 'mmanhaes.com.br',
-    isExternal: true,
-  },
+  // {
+  //   theme: 'outline',
+  //   label: 'Portfolio',
+  //   icon: 'fa fa-user-circle',
+  //   route: 'mmanhaes.com.br',
+  //   isExternal: true,
+  // },
   {
     theme: 'outline',
     label: 'GitHub Search',
@@ -21,11 +21,11 @@ export const HOME: IHome[] = [
     route: '/gh-search',
     isExternal: false,
   },
-  {
-    theme: 'outline',
-    label: 'Gerenciador de Figurinhas',
-    icon: 'fa fa-sticky-note',
-    route: 'gerenciador-figurinhas.netlify.app',
-    isExternal: true,
-  },
+  // {
+  //   theme: 'outline',
+  //   label: 'Gerenciador de Figurinhas',
+  //   icon: 'fa fa-sticky-note',
+  //   route: 'gerenciador-figurinhas.netlify.app',
+  //   isExternal: true,
+  // },
 ];
