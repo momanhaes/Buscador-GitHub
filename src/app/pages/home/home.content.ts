87bc5b31b8501344e82624c61b4ed4ef5e37081b
mmanhaes <momanhaes@gmail.com>
2023-04-21 15:44:40 -0300

    feat: update portfolio

--- src/app/pages/home/home.content.ts
+++ src/app/pages/home/home.content.ts
@@ -7,25 +7,60 @@ export interface IHome {
 }
 
 export const HOME: IHome[] = [
-  // {
-  //   theme: 'outline',
-  //   label: 'Portfolio',
-  //   icon: 'fa fa-user-circle',
-  //   route: 'mmanhaes.com.br',
-  //   isExternal: true,
-  // },
-  // {
-  //   theme: 'outline',
-  //   label: 'GitHub Search',
-  //   icon: 'fa fa-github',
-  //   route: '/gh-search',
-  //   isExternal: false,
-  // },
-  // {
-  //   theme: 'outline',
-  //   label: 'Gerenciador de Figurinhas',
-  //   icon: 'fa fa-sticky-note',
-  //   route: 'gerenciador-figurinhas.netlify.app',
-  //   isExternal: true,
-  // },
+  {
+    theme: 'outline',
+    label: 'Portfolio',
+    icon: 'fa fa-user-circle',
+    route: 'mmanhaes.com.br',
+    isExternal: true,
+  },
+  {
+    theme: 'outline',
+    label: 'GitHub Search',
+    icon: 'fa fa-github',
+    route: '/gh-search',
+    isExternal: false,
+  },
+  {
+    theme: 'outline',
+    label: 'JS Vanilla GitHub Search',
+    icon: 'fa fa-github-square',
+    route: 'mmanhaes-jsgh-search.netlify.app',
+    isExternal: true,
+  },
+  {
+    theme: 'outline',
+    label: 'Gerenciador de Figurinhas',
+    icon: 'fa fa-sticky-note',
+    route: 'gerenciador-figurinhas.netlify.app',
+    isExternal: true,
+  },
+  {
+    theme: 'outline',
+    label: 'Wedding Site',
+    icon: 'fa fa-diamond',
+    route: 'casamentomem.netlify.app',
+    isExternal: true,
+  },
+  {
+    theme: 'outline',
+    label: 'Calculadora',
+    icon: 'fa fa-calculator',
+    route: 'mmanhaes-calculadora.netlify.app',
+    isExternal: true,
+  },
+  {
+    theme: 'outline',
+    label: 'To Do List',
+    icon: 'fa fa-list-ul',
+    route: 'mmanhaes-todolist.netlify.app',
+    isExternal: true,
+  },
+  {
+    theme: 'outline',
+    label: 'CRUD Produtos',
+    icon: 'fa fa-plus-square',
+    route: 'mmanhaes-crud-produtos.netlify.app',
+    isExternal: true,
+  },
 ];
