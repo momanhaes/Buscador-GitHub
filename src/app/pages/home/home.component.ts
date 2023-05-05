f51ca9bc3bc104d3b038207c9456a2ef9dceb077
mmanhaes <momanhaes@gmail.com>
2023-05-05 17:26:01 -0300

    feat: atualização da home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { APPEARD } from 'src/app/shared/animations/appeard.animation';
 import { LIST_ANIMATION_LATERAL } from 'src/app/shared/animations/list.animation';
+import { ISocialContent, SOCIAL_CONTENT } from './home.content';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +12,7 @@ import { LIST_ANIMATION_LATERAL } from 'src/app/shared/animations/list.animation
 export class HomeComponent implements OnInit {
   public state = 'ready';
   public show!: boolean;
+  public content: ISocialContent[] = SOCIAL_CONTENT;
 
   constructor() {}
 
@@ -18,5 +20,12 @@ export class HomeComponent implements OnInit {
     setTimeout(() => {
       this.show = true;
     }, 0);
+  }
+
+  public goTo(url: string): void {
+    let URL: string = '';
+    if (!/^http[s]?:\/\//.test(url)) { URL += 'http://'; }
+    URL += url;
+    window.open(URL, '_blank');
   }
 }
---
+++ src/app/pages/home/home.content.ts
@@ -0,0 +1,33 @@
+export interface ISocialContent {
+  link: string;
+  color: string;
+  svg: string;
+}
+
+export const SOCIAL_CONTENT: ISocialContent[] = [
+  {
+    link: 'https://www.linkedin.com/in/mmanhaes/',
+    color: 'primary',
+    svg: '/assets/svgs/linkedin.svg',
+  },
+  {
+    link: 'https://github.com/momanhaes',
+    color: 'primary',
+    svg: '/assets/svgs/github.svg',
+  },
+  {
+    link: 'https://twitter.com/momanhaes',
+    color: 'primary',
+    svg: '/assets/svgs/twitter.svg',
+  },
+  {
+    link: 'https://www.facebook.com/momanhaes',
+    color: 'primary',
+    svg: '/assets/svgs/facebook.svg',
+  },
+  {
+    link: 'https://www.instagram.com/manhaesdev/',
+    color: 'primary',
+    svg: '/assets/svgs/instagram.svg',
+  },
+];
